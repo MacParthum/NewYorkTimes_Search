9f1c8e56fe88cc2c3f2ed671b9edf46a1b67258d
Mac Parthum <macparthum@gmail.com>
2020-08-16 01:46:26 -0400

    got the api attached to the search button and console logging!

--- logic.js
+++ logic.js
@@ -1,1 +1,24 @@
-alert("test");
+var authKey ="3BjhUi86GMAIrd3GTGFH5vCPIlKCcATh";
+
+var queryTerm  ="";
+var numResults = 0;
+var startYear  = 0;
+var endYear     =0;
+
+var queryURLBase = "http://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + authKey;
+
+var articleCounter = 0;
+
+function runQuery(numArticles, queryUrl) {
+    $.ajax({url: queryUrl, method: "GET"})
+    .done(function(NYTData) {
+       console.log(NYTData);
+    })
+
+}
+
+$('#searchBtn').on('click', function() {
+    runQuery(10, "http://api.nytimes.com/svc/search/v2/articlesearch.json?&api-key=3BjhUi86GMAIrd3GTGFH5vCPIlKCcATh&q=Obama");
+    
+    return false;
+})
