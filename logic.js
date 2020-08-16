c6dda3fb9be23da4fbf095788a6c0447fcd22f9a
Mac Parthum <macparthum@gmail.com>
2020-08-16 11:16:59 -0400

    got the api working and console logging multiple articles

--- logic.js
+++ logic.js
@@ -1,24 +1,34 @@
-var authKey ="3BjhUi86GMAIrd3GTGFH5vCPIlKCcATh";
+var authKey = "3BjhUi86GMAIrd3GTGFH5vCPIlKCcATh";
 
-var queryTerm  ="";
+var queryTerm = "";
 var numResults = 0;
-var startYear  = 0;
-var endYear     =0;
+var startYear = 0;
+var endYear = 0;
 
 var queryURLBase = "http://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + authKey;
 
 var articleCounter = 0;
 
-function runQuery(numArticles, queryUrl) {
-    $.ajax({url: queryUrl, method: "GET"})
-    .done(function(NYTData) {
-       console.log(NYTData);
-    })
+function runQuery(numArticles, queryURL) {
+
+    $.ajax({ url: queryURL, method: "GET" })
+        .done(function (NYTData) {
+            console.log(queryURL);
+            console.log(NYTData);
+
+        })
 
 }
 
-$('#searchBtn').on('click', function() {
-    runQuery(10, "http://api.nytimes.com/svc/search/v2/articlesearch.json?&api-key=3BjhUi86GMAIrd3GTGFH5vCPIlKCcATh&q=Obama");
-    
+$('#searchBtn').on('click', function () {
+
+    queryTerm = $('#search').val().trim();
+    console.log(queryTerm);
+
+    var newURL = queryURLBase + "&q=" + queryTerm;
+    console.log(newURL);
+
+    runQuery(10, newURL);
+
     return false;
 })
