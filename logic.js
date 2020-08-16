c303ff29718d7c1701e1ca8a6e674c6b7bea999a
Mac Parthum <macparthum@gmail.com>
2020-08-16 14:41:00 -0400

    console logging all the docs and working properly

--- logic.js
+++ logic.js
@@ -5,7 +5,7 @@ var numResults = 0;
 var startYear = 0;
 var endYear = 0;
 
-var queryURLBase = "http://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + authKey;
+var queryURLBase = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + authKey;
 
 var articleCounter = 0;
 
@@ -13,22 +13,46 @@ function runQuery(numArticles, queryURL) {
 
     $.ajax({ url: queryURL, method: "GET" })
         .done(function (NYTData) {
+
+
+            for (var i = 0; i <numArticles; i++) {
+                console.log(NYTData.response.docs[i].headline.main);
+                console.log(NYTData.response.docs[i].section_name);
+                console.log(NYTData.response.docs[i].pub_date);
+                console.log(NYTData.response.docs[i].byline.original);
+                console.log(NYTData.response.docs[i].web_url);
+            }
+
             console.log(queryURL);
+            console.log(numArticles);
             console.log(NYTData);
-
         })
-
 }
 
 $('#searchBtn').on('click', function () {
 
     queryTerm = $('#search').val().trim();
-    console.log(queryTerm);
 
     var newURL = queryURLBase + "&q=" + queryTerm;
-    console.log(newURL);
 
-    runQuery(10, newURL);
+    numResults = $('#numRecords').val();
+
+    startYear = $('#startYear').val().trim();
+    endYear = $('#endYear').val().trim();
+
+    if (parseInt(startYear)) {
+        startYear = startYear + "0101";
+
+        newURL = newURL + "&begin_date=" + startYear;
+    }
+
+    if (parseInt(endYear)) {
+        endYear = endYear + "0101";
+
+        newURL = newURL + "&end_date=" + endYear;
+    }
+
+    runQuery(numResults, newURL);
 
     return false;
 })
